58618feaa43a99ae4ce284ea48edf7fda063bd4e
jcghun <jucgomezhi@unal.edu.co>
2022-09-01 12:10:08 -0500

    Ejemplo punteros
    Se realiza el primer ejemplo para indicar el funcionamiento de los punteros

--- BasicConfig/Src/main.c
+++ BasicConfig/Src/main.c
@@ -18,8 +18,30 @@
  */
 
 #include <stdint.h>
+#include "stm32f411xx_hal.h"
+#include "GPIOxDriver.h"
+
+uint8_t dummy = 0;
+uint8_t *prtEjemplo;
+uint32_t valorPuntero = 0;
 
 int main(void){
+
+	//Cambiando el valor de una variable
+	dummy = 30;
+
+	//Haciendo que el puntero apunte a la variable dummy
+	ptrEjemplo = &dummy;
+
+	//Modificando de forma indirecta el valor alamacenado en la casilla a donde apunta el puntero
+	*ptrEjemplo += 45;
+
+	//Observando el valor de posición de memoria
+	valorPuntero = (int)ptrEjemplo;
+
+	ptrEjemplo++;
+	*ptrEjemplo = 0xAC;
+
     /* Loop forever */
 	while(1){
 
