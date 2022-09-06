16cbc90d3b4518999f32b7cb3027229b7de12cde
jcghun <jucgomezhi@unal.edu.co>
2022-09-05 22:30:24 -0500

    Creación Tarea 2
    Se crea y configura el proyecto para la tarea 2

--- SolucionTarea2/Src/main.c
+++ SolucionTarea2/Src/main.c
@@ -2,7 +2,8 @@
  ******************************************************************************
  * @file           : main.c
  * @author         : Juan Camilo Gomez Hidalgo
- * @brief          : Configuración inicial
+ * @email		   : jucgomezhi@unal.edu.co
+ * @brief          : Main program body
  ******************************************************************************
  * @attention
  *
@@ -18,33 +19,9 @@
  */
 
 #include <stdint.h>
-#include "stm32f411xx_hal.h"
-#include "GPIOxDriver.h"
 
-uint8_t dummy = 0;
-uint8_t *ptrEjemplo;
-uint32_t valorPuntero = 0;
-
-int main(void){
-
-	//Cambiando el valor de una variable
-	dummy = 30;
-
-	//Haciendo que el puntero apunte a la variable dummy
-	ptrEjemplo = &dummy;
-
-	//Modificando de forma indirecta el valor alamacenado en la casilla a donde apunta el puntero
-	*ptrEjemplo += 45;
-
-	//Observando el valor de posición de memoria
-	valorPuntero = (int)ptrEjemplo;
-
-	ptrEjemplo++;
-	*ptrEjemplo = 0xAC;
-
+int main(void)
+{
     /* Loop forever */
-	while(1){
-
-	}
-	return 0;
+	for(;;);
 }
