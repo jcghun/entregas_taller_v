e25f0dda8e61cee6e00a3c726763d7f1a998b680
Nerio Andrés Montoya G <namontoy@unal.edu.co>
2022-12-01 00:38:39 -0500

    Revisión Tarea 5
    Calificación = 3.1

--- PeripheralDriver/Src/USARTxDriver.c
+++ PeripheralDriver/Src/USARTxDriver.c
@@ -288,3 +288,12 @@ __attribute__ ((weak)) void usart2Rx_Callback(void){
 __attribute__ ((weak)) void usart6Rx_Callback(void){
 	__NOP();
 }
+
+/*
+ * Juan Camilo, sobre tu configuración:
+ * - Hay una mezcla confusa entre valores para 16MHz y 100MHz.
+ * - Deberias tener una forma de seleccionar los valores con respecto a la velocidad del procesador que tengas activa.
+ * - No tienes en cuenta que el bus APB1 (USART1 y USART6) corre mas rapido que el APB2 (USART2), por lo cual toda tu
+ *   configuracion queda solo sirviendo para el USART2, los otros dos dejan de funcionar.
+ *  Calificación = 3.1
+ * */
